c5f786cb3ea07a2b53f5e4935f05b9a289d5e49b
Marcelo <marc97ss@gmail.com>
2021-03-30 13:37:41 -0600

    Added motion sensor and I2C Arduino Code and Simulation

--- ArduinoPrincipal/ArduinoPrincipal.ino
+++ ArduinoPrincipal/ArduinoPrincipal.ino
@@ -1,24 +1,59 @@
-#define TEMPERATURE_PIN 0
-#define SOUND_PIN 1
-#define MOVEMENT_PIN 2
-#define TEMP_PIN 3
+#include <Wire.h>
 
+#define TEMPERATURE_PIN 0 // analog
+#define SOUND_PIN 1 // analog
+#define MOVEMENT_PIN 2 // digital
+#define TIMER_PIN 3
+#define PIR_LED 13 // digital
+
+// variable declaration 
 double temperature_measure = 0;
 double sound_measure = 0;
+int movement_sensor_state = LOW;
+int movement_measure = 0;
 
 void setup() {
+  Wire.begin();
   Serial.begin(9600);
+  pinMode(MOVEMENT_PIN, INPUT);
+  pinMode(PIR_LED, OUTPUT);
 }
 
 void loop() {
 
   temperature_measure = analogRead(TEMPERATURE_PIN);
   sound_measure = analogRead(SOUND_PIN);
-  Serial.println("Temp: ");
+  movement_measure = digitalRead(MOVEMENT_PIN);
+
+  if(movement_measure == HIGH) {
+    digitalWrite(PIR_LED, HIGH);
+    if(movement_sensor_state == LOW) {      
+      Serial.println("Motion detected...");
+      movement_sensor_state = HIGH;
+      // I2C Transmission test
+      Wire.beginTransmission(9); // to slave addr = 9
+      Wire.write(movement_sensor_state);              
+      Wire.endTransmission();
+      // end of test
+    }
+  } else {
+    digitalWrite(PIR_LED, LOW);
+    if(movement_sensor_state == HIGH) {
+      Serial.println("Motion ended...");
+      movement_sensor_state = LOW;
+      // I2C Transmission test
+      Wire.beginTransmission(9); // to slave addr = 9
+      Wire.write(movement_sensor_state);              
+      Wire.endTransmission();
+      // end of test
+    }
+  }
+  
+  /*Serial.println("Temp: ");
   Serial.println(temperature_measure);
   Serial.println();
   Serial.println("Sound: ");
   Serial.println(sound_measure);
   Serial.println("----------------");
-  delay(1000);
+  delay(1000);*/
 }
--- ArduinoSecundario/ArduinoSecundario.ino
+++ ArduinoSecundario/ArduinoSecundario.ino
@@ -1,4 +1,5 @@
 #include <Servo.h> 
+#include <Wire.h>
 
 #define LED_R 13
 #define LED_G 12
@@ -8,35 +9,41 @@
 Servo myservo4; 
 Servo myservo3; 
 int pos = 0;
-double sensorRead = 0;    
+double sensorRead = 0;  
+int I2CRead = LOW;  
 
 void setup() 
-{ 
-  // serial setup for debugging
-  Serial.begin(9600);
+{
+    // serial setup for debugging
+   Serial.begin(9600);
+  
+    // servos
+   myservo4.attach(4);
+   myservo3.attach(3);
 
-  // servos
-  myservo4.attach(4);
-  myservo3.attach(3);
-
- // leds 
- pinMode(LED_R, OUTPUT);
- pinMode(LED_G, OUTPUT);
- pinMode(LED_B, OUTPUT);
+   Wire.begin(9); 
+   Wire.onReceive(receiveEvent);
+  
+     // leds 
+   pinMode(LED_R, OUTPUT);
+   pinMode(LED_G, OUTPUT);
+   pinMode(LED_B, OUTPUT);
 }
  
 void loop() 
 { 
   sensorRead = analogRead(LIGHT_PIN);
-  Serial.println("Reading: ");
+  /*Serial.println("Reading: ");
   Serial.print(sensorRead);
-  Serial.println(" ");
+  Serial.println(" ");*/
 
-  digitalWrite(LED_R, HIGH);
-  digitalWrite(LED_G, HIGH);
-  digitalWrite(LED_B, HIGH);
-  
-  for(pos = 0; pos <= 180; pos += 1) 
+  if(I2CRead == HIGH) {
+    digitalWrite(LED_R, HIGH);
+  } else {
+    digitalWrite(LED_R, LOW);
+  }
+
+  /*for(pos = 0; pos <= 180; pos += 1) 
   {
     myservo4.write(pos);
     myservo3.write(pos);              
@@ -52,5 +59,10 @@ void loop()
     myservo4.write(pos);
     myservo3.write(pos);           
     delay(15);                      
-  }
+  }*/
+}
+
+void receiveEvent(int bytes) 
+{
+  I2CRead = Wire.read();
 }
