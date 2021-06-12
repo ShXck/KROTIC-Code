9f647d0a4af1d7fe1a342e95d1eacfef9109df2e
ShXck <marc97ss@gmail.com>
2021-06-11 22:19:53 -0600

    Arduino code updated

--- ArduinoPrincipal/ArduinoPrincipal.ino
+++ ArduinoPrincipal/ArduinoPrincipal.ino
@@ -2,17 +2,13 @@
 #include <Servo.h> 
 
 // CONSTANTS
-#define SERVO_MOV_TIME 1500
-#define BATTERY_TH 460
+#define SERVO_MOV_TIME 1250
 
-// ANALOG INPUT
-#define TEMPERATURE_PIN 0
-#define BATTERY_PIN 1
-
-
-
-// STATE LEDS
-#define BATTERY_LED 2
+// ANALOG INPUTS
+#define LIGHT_SENSOR_PIN 0
+#define TEMP_SENSOR_PIN 1
+#define MOTION_SENSOR_PIN 2
+#define SOUND_SENSOR_PIN 3
 
 // INSTRUCTIONS AVAILABLE
 #define READ_LIGHT_INSTR 0
@@ -21,16 +17,23 @@
 #define READ_SOUND_INSTR 3
 #define MOVE_FORWARD 4
 #define MOVE_BACKWARDS 5
+#define ROTATE_RIGHT 6
+#define ROTATE_LEFT 7
 
 Servo servo1;
 Servo servo2;
 
-// variable declaration 
-int temperature_measure = 0;
-int battery_level = 0;
+// Analog readings
+
+int temperature_measure = 0; // temperature sensor reading
+int light_measure = 0;       // light sensor reading
+int motion_measure = 0;      // motion sensor reading
+int sound_measure = 0;       // sound sensor reading
+
+
+// I2C
 int I2CRead = -1; // instruction read
-byte sent_data;
-bool isLow;
+byte sent_data;   // data to be sent to the raspberry upon request
 
 void setup() {
   // Declares arduino as slave using address 0x8
@@ -40,36 +43,40 @@ void setup() {
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 
+  // attach servos to digital pins 9 and 10
   servo1.attach(9);
   servo2.attach(10);
 
+  // stop the servos from the begginning
   servo1.write(90);
   servo2.write(90);
   
   Serial.begin(9600);
-  
-  pinMode(BATTERY_LED, OUTPUT);
-  digitalWrite(BATTERY_LED, LOW);
-
-  isLow = false;
 }
 
 void loop() {
   temperature_measure = analogRead(TEMPERATURE_PIN);
-  battery_level = analogRead(BATTERY_PIN);
-
-  Serial.println(battery_level);
-
-  sent_data = map(temperature_measure, 0, 1023, 0, 255);
+  light_measure = analogRead(READ_LIGHT_INSTR);       
+  motion_measure = analogRead(MOTION_SENSOR_PIN);     
+  sound_measure = analogRead(SOUND_SENSOR_PIN); 
 
   switch(I2CRead) {
     case READ_LIGHT_INSTR:
-    case READ_TEMP_INSTR:
-    case READ_MOTION_INSTR:
-    case READ_SOUND_INSTR:
-      sent_data = map(temperature_measure, 0, 1023, 0, 255);
+      sent_data = map(light_measure, 0, 1023, 0, 255);        // map it to only send 8 bits.
       I2CRead = -1;
       break;
+    case READ_TEMP_INSTR:
+      sent_data = map(temperature_measure, 0, 1023, 0, 255); // map it to only send 8 bits.
+      I2CRead = -1;
+      break;
+    case READ_MOTION_INSTR:
+      sent_data = map(motion_measure, 0, 1023, 0, 255);      // map it to only send 8 bits.
+      I2CRead = -1;
+      break;
+    case READ_SOUND_INSTR:
+      sent_data = map(sound_measure, 0, 1023, 0, 255);       // map it to only send 8 bits.
+      I2CRead = -1;
+      break;  
     case MOVE_FORWARD:
       move_forward();
       I2CRead = -1;
@@ -78,8 +85,19 @@ void loop() {
       move_backwards();
       I2CRead = -1;
       break;
+    case ROTATE_RIGHT:
+      rotate_right();
+      I2CRead = -1;
+      break;
+    case ROTATE_LEFT:
+      rotate_left();
+      I2CRead = -1;
+      break;
   }
 
+  /**
+   * Test purposes only, use the serial monitor to test servo movement.
+   */
   if(Serial.available() > 0) {
     char in = Serial.read();
     if(in == '1') {
@@ -89,12 +107,14 @@ void loop() {
     } else if(in == '3') {
       move_backwards();
     } else if(in == '4') {
-      rotate();
+      rotate_right();
+    } else if(in == '5'){
+      rotate_left();
     }
   }
 
   // warns about low servo battery
-  if(battery_level <= BATTERY_TH) {
+  /*if(battery_level <= BATTERY_TH) {
     if(isLow) {
       digitalWrite(BATTERY_LED, HIGH);
       isLow = false;
@@ -102,9 +122,7 @@ void loop() {
       digitalWrite(BATTERY_LED, LOW);
       isLow = true;
     }
-  }
-  
-  delay(1000);
+  }*/
 }
 
 void move_forward() {
@@ -122,10 +140,17 @@ void move_backwards() {
   stop_movement();               
 }
 
-void rotate() {
+void rotate_right() {
   servo1.write(180);
   servo2.write(180);
-  delay(SERVO_MOV_TIME);
+  delay(225);
+  stop_movement();               
+}
+
+void rotate_left() {
+  servo1.write(0);
+  servo2.write(0);
+  delay(200);
   stop_movement();               
 }
 
@@ -134,18 +159,18 @@ void stop_movement() {
   servo2.write(90);              
 }
 
+/**
+ * This function is triggered when the Raspberry sends an instruction.
+ */
 void receiveEvent(int bytes) 
 {
   I2CRead = Wire.read();
 }
 
-void requestEvent(int bytes) {
-  switch(I2CRead) {
-    case READ_LIGHT_INSTR:
-    case READ_TEMP_INSTR:
-    case READ_MOTION_INSTR:
-    case READ_SOUND_INSTR:
-      Wire.write(sent_data);
-      break;
-  }
+/**
+ * This function is triggered when the Raspberry request the value of a sensor.
+ */
+void requestEvent(int bytes) 
+{
+  Wire.write(sent_data); // send requested data.
 }
